cb541fdeb87a682f11d37ac3ff5ae662427c801e
Akashneel@arch <akashneelrc@kgpian.iitkgp.ac.in>
2025-09-22 18:53:10 +0530

    Add prod API ip

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 import { isProd } from "@/utils"
 
-const API_BASE_URL = isProd ? "" : "http://localhost:8000";
+const API_BASE_URL = isProd ? "http://13.60.42.199:8000" : "http://localhost:8000";
 
 export {API_BASE_URL};
